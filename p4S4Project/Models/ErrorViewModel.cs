6085c3fdc971af592d2f6b7e87660c31b25b7d48
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-09-20 11:18:30 +0530

    first commit

---
+++ p4S4Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace p4S4Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
